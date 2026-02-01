ba9590200461a552b3b17b9d3f1a99be9835c226
priyob12-blip <priyob12@gmail.com>
2026-02-01 22:00:44 +0800

    Update safety.py

--- safety.py
+++ safety.py
@@ -72,7 +72,7 @@ if shape == "Trapesium":
             
             vol_efektif_bund = vol_bruto - vol_pond_tank
 
-            if vol_efektif_bund < batas_aman:
+            if vol_pond_tank < vol_efektif_bund:
                 status = "✗ NON COMPLY - Volume bund kurang"
             elif tinggi_dinding > 1.8:
                 status = "✗ NON COMPLY - Tinggi dinding > 1,8 m"
